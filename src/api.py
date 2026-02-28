109ea28c6664b11bbd1bd6c27d621dc07f047333
Jeevan <jeevu9312@gmail.com>
2026-02-28 13:54:49 +0530

    Fix deployment configuration

--- src/api.py
+++ src/api.py
@@ -1,31 +1,30 @@
-# src/api.py
-
 import os
 import pickle
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(BASE_DIR, "..", "models", "salary_model.pkl")
 
-with open(MODEL_PATH, "rb") as f:
-    model = pickle.load(f)
-
+model = pickle.load(open(MODEL_PATH, "rb"))
 
 @app.get("/")
 def home():
-    return {"message": "Salary Prediction API is running"}
-
+    return {"status": "API working"}
 
 @app.post("/predict")
-def predict_salary(data: dict):
-
-    input_df = pd.DataFrame([data])
-
-    prediction = model.predict(input_df)[0]
-
-    return {
-        "predicted_salary": float(prediction)
-    }
+def predict(data: dict):
+    df = pd.DataFrame([data])
+    prediction = model.predict(df)[0]
+    return {"predicted_salary": float(prediction)}
